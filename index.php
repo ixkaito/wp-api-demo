4c919a9d9721d97b7093391c016e2f6328dfa9cb
KITE <ixkaito@gmail.com>
2014-09-05 17:35:05 +0900

    Add filters

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
-$url  = 'http://localhost/wordpress1/wp-json/posts/2069';
+$args = array(
+	'filter[posts_per_page]' => 20,
+	'filter[orderby]'        => 'title',
+	'filter[order]'          => 'ASC'
+);
+$url  = add_query_arg( $args, 'http://localhost/wordpress1/wp-json/posts' );
 $data = slug_get_json( $url );
 
-var_dump( $data );
+foreach ($data as $key => $post) {
+	echo $post->title . '<br />';
+}
